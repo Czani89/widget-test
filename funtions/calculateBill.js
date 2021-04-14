1fd597cb71d27910a3818a106727227d673894fa
Cinga <czani89@gmail.com>
2021-04-14 11:30:44 +0200

    added calc bill

--- funtions/calculateBill.js
+++ funtions/calculateBill.js
@@ -1,20 +1,25 @@
-var callTotal = 0;
-var smsTotal = 0;
-var total = 0;
-var billItems;
-function calculateBill(bill) {
-  billItems = bill.split(",");
+function calcBill() {
+  var total = 0;
 
-  for (let i = 0; billItems.length; i++) {
-    var billType = billItems[i].trim();
-    if (billType === "call") {
-      callTotal += 2.75;
-    }
-    if (billType === "sms") {
-      smsTotal += 0.75;
+  function calculateBill(bill) {
+    var billItems = bill.split(",");
+
+    for (let i = 0; i < billItems.length; i++) {
+      var billType = billItems[i].trim();
+      if (billType === "call") {
+        total += 2.75;
+      }
+      if (billType === "sms") {
+        total += 0.75;
+      }
     }
   }
-  total = callTotal + smsTotal;
+  function bTotal() {
+    return total;
+  }
 
-  return total;
+  return {
+    calculateBill,
+    bTotal,
+  };
 }
--- tests/calculateTest.js
+++ tests/calculateTest.js
@@ -1,5 +1,22 @@
 describe("Calculate bill", function () {
   it("should show you the  bill for sms(0,75)", function () {
-    assert.equal(0.75, calculateBill("sms, sms"));
+    var callB = calcBill();
+    callB.calculateBill("sms");
+    assert.equal(0.75, callB.bTotal());
+  });
+  it("should show you the  bill for call(2,75)", function () {
+    var callB = calcBill();
+    callB.calculateBill("call");
+    assert.equal(2.75, callB.bTotal());
+  });
+  it("should show you the  bill for call(2,75) and sms(0.75)", function () {
+    var callB = calcBill();
+    callB.calculateBill("call, sms");
+    assert.equal(3.5, callB.bTotal());
+  });
+  it("should show you the total bill for a string of calls and sms", function () {
+    var callB = calcBill();
+    callB.calculateBill("call, sms, call, sms, call, sms, call, sms");
+    assert.equal(14, callB.bTotal());
   });
 });
